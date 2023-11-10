cc7472c7060d2b85950ab00cb33b0de32c0bd551
AdamCichoski <cjadam003@gmail.com>
2023-11-10 12:15:30 -0500

    Output organization to master branch

--- src/calculator/ProbabilityCalculator.java
+++ src/calculator/ProbabilityCalculator.java
@@ -9,11 +9,13 @@ public class ProbabilityCalculator {
 		double event1=0.1;
 		double event2=0.5;
 		Probability p = new Probability(event1,event2);
-		System.out.printf("%-21s = %5f\n", "P(event1 or event2)",(p.or(event1, event2)));
-		System.out.printf("%-21s = %5f\n", "P(event1 and event2)", (p.and(event1, event2)));
-		System.out.printf("%-21s = %5f\n", "P(~event1)", (p.not(event1)));
-		System.out.printf("%-21s = %5f\n", "P(~event1 or ~event2)", (p.not(event1, event2)));
-		System.out.printf("%-21s = %5f\n", "P(event1 | event2)", (p.given(event1, event2)));
+		System.out.println("Probability Calculations:");
+
+		System.out.printf("%-21s = %5.3f\n", "P(event1 or event2)",(p.or(event1, event2)));
+		System.out.printf("%-21s = %5.3f\n", "P(event1 and event2)", (p.and(event1, event2)));
+		System.out.printf("%-21s = %5.3f\n", "P(~event1)", (p.not(event1)));
+		System.out.printf("%-21s = %5.3f\n", "P(~event1 or ~event2)", (p.not(event1, event2)));
+		System.out.printf("%-21s = %5.3f\n", "P(event1 | event2)", (p.given(event1, event2)));
 		
 		System.out.println("");
 
@@ -22,5 +24,7 @@ public class ProbabilityCalculator {
 		System.out.println("P(~event1) = " + String.format("%.1f", p.not(event1) * 100) + '%');
 		System.out.println("P(~event1 or ~event2) = " + String.format("%.1f", p.not(event1, event2) * 100) + '%');
 		System.out.println("P(event1 | event2) = " + String.format("%.1f", p.given(event1, event2) * 100) + '%');
+
+		System.out.println("Combinatorics Calculations:");
 	}
 }
