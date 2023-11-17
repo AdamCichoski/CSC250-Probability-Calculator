e4d2d39b86d5735158d1908f7039a43dd2d2da0c
AdamCichoski <cjadam003@gmail.com>
2023-11-17 12:21:20 -0500

    Add Cards Class, Programmed Combination Class

--- src/calculator/Combination.java
+++ src/calculator/Combination.java
@@ -6,5 +6,34 @@ package calculator;
  * @Author Adam Cichoski, Natalie Hildreth, Bennet Scott, Joseph Holly, Alex Bonaker
  */
 public class Combination {
-    private double event1;
+    private int ntotal;
+    private int rset;
+
+    /**
+     * This is the constructor for this class
+     * @param ntotal
+     * @param rset
+     */
+    Combination(int ntotal, int rset){
+        this.ntotal=ntotal;
+        this.rset=rset;
+    }
+    /**
+     * Calculates the combination
+     * @return the double value of the combination
+     */
+    public double calculate(){
+        return (factorial(this.ntotal))/((double)((factorial(this.rset))*(factorial(this.ntotal-this.rset))));
+    }
+
+    private int factorial(int x){
+        int total=1;
+        if(x<0){
+            return -1;
+        }
+        for(int i=x;i>0;i--){
+            total*=i;
+        }
+        return total;
+    }
 }
